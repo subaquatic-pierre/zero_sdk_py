3cb56e1e35f1fa5d2c57bf022c1e4fbce228f46a
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-08-01 15:19:59 +0400

    Updated default network config

--- zero_sdk/config.py
+++ zero_sdk/config.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from zero_sdk.utils import from_json
 
 PROJECT_ROOT = Path(__file__).resolve().parent.resolve().parent.resolve()
+PUBLIC_URL = "https://beta.0chain.net"
 
+# Change default paths to home dir in production
 default_network_obj = from_json(
     os.path.join(PROJECT_ROOT, "default_config/network.json")
 )
-
 default_wallet_obj = from_json(os.path.join(PROJECT_ROOT, "default_config/wallet.json"))
