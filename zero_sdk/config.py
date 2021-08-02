944c3f615f7357e3c1bfc73964ae60221beee208
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-08-02 10:42:04 +0400

    Removed default_conf directory, import config settings from home dir;

--- zero_sdk/config.py
+++ zero_sdk/config.py
@@ -1,14 +1,11 @@
 import os
 from pathlib import Path
-from zero_sdk.utils import from_json, from_yaml
+from zero_sdk.utils import from_json, from_yaml, get_home_path
 
 PROJECT_ROOT = Path(__file__).resolve().parent.resolve().parent.resolve()
+HOME_PATH = get_home_path()
 DEFUALT_PUBLIC_URL = "https://beta.0chain.net"
 
 # Change default paths to home dir in production
-default_network_config_obj = from_yaml(
-    os.path.join(PROJECT_ROOT, "zero_sdk/default_config/network.yaml")
-)
-default_wallet_config_obj = from_json(
-    os.path.join(PROJECT_ROOT, "zero_sdk/default_config/wallet.json")
-)
+default_network_config_obj = from_yaml(os.path.join(HOME_PATH, ".zcn/config.yaml"))
+default_wallet_config_obj = from_json(os.path.join(HOME_PATH, ".zcn/wallet.json"))
